4b3b605739cceeda7040e552a5b35d19af83af66
SUJEET KUMAR SINGH <127024459+Sujeetksingh222@users.noreply.github.com>
2023-09-29 20:47:10 +0000

    data types

---
+++ 01_basicjs/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict";// treat all js code as newer version
+
+//alert("Hello")// we are using node js ,not browser
+console.log(3+3)
+console.log(3
+    +4)// code readability should be high
+
+console.log("sujeet")
+// for alert(), please refer alert mdn documentation
+
+
+let name ="sujeet"
+let age = 24
+let isLoggedIn = false
+
+
+// data types:-
+//primitive datatype
+//1. number => range 2 to power 53
+// bigint is used to store large value
+// 2.string=>""
+//3. boolean=>true/false
+//4.null =>standalone value i.e empty
+//5.undefined=> value not defined i.e  variable is defined but we have not assigned any value
+//6. symbol =>used for uniqueness
+
+
+// 7.object 
+// typeof(): used to check data type of variable
+console.log(typeof("sujeet"))
+console.log(typeof(null))// type of null is object
+console.log(typeof(undefined))// type of undefined is undefined.

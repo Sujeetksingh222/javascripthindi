e5ee55b1997490df718bc5a8c88caacd04b7550a
SUJEET KUMAR SINGH <127024459+Sujeetksingh222@users.noreply.github.com>
2023-10-09 20:44:55 +0000

    date and time

---
+++ 01_basicjs/07_Dates.js
@@ -0,0 +1,41 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,9,10);
+// console.log(myCreatedDate.toDateString());
+// // in javascript month starts from zero
+
+
+// let myCreatedDate = new Date(2023,9,10,5,3);
+let myCreatedDate = new Date("2023-10-10");
+console.log(myCreatedDate.toLocaleString());
+
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// // console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor((Date.now())/1000));
+
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth()+1);
+
+
+console.log(`${newDate.getDay()} and the time`);
+
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    
+})

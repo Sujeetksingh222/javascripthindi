a8c7785805aa572181b4097b2e53d23beb69c9ad
SUJEET KUMAR SINGH <127024459+Sujeetksingh222@users.noreply.github.com>
2023-12-22 18:58:00 +0000

    variable in js

---
+++ Javascriptbyapnacollege/lecture1.js
@@ -0,0 +1,29 @@
+console.log("sujeet kumar singh")
+fullName="sachin Tendulakar"
+age=24
+price=99.9
+// x=null
+// y=undefined
+// console.log(y);
+console.log(age);
+console.log(fullName);
+console.log(price);
+// console.log(x);
+var Name="sujeet kumar singh"
+console.log(Name);
+Name="Rohan kumar"
+console.log(Name);
+let x=19
+console.log(x);
+x=20
+console.log(x);
+const y=10
+console.log(y);
+// y=39
+const student={
+    fullname:"sujeet kumar singh",
+    age:24,
+    cgpa:8.13,
+    ispass:true
+}
+console.log(student.age);
